7d2ed0b5b8798de927a66321d8f76cf63a3f92ca
unknown <tkacnikita716@gmail.com>
2021-10-06 10:58:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// let number = 5;
+
+// const leftBorderWidth = 1; 
+
+// number = 9;
+// console.log(number);
+
+// alert('nikita');
+
+// const result = confirm("nikita?");
+// console.log(result);
+
+// const answer = prompt("вам 18 лет ?", "45");
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('Как вас зовут');
+answers[1] = prompt('Как вас зовут2222222');
+answers[2] = prompt('Как вас зовут333333333');
+
+document.write(answers);
